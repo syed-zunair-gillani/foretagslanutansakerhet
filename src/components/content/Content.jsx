815d7d1733937247faf93cf7c969f84c82c6b6bf
syed zunair gillani <zunairgillani54@gmail.com>
2023-12-05 01:25:18 +0500

    content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,55 +5,19 @@ const Content = () => {
   return (
     <section className="mt-20">
       <Container className="flex mb-20">
-        <div class="flex w-full">
-          <aside class="h-screen sticky top-24 w-2/6">
-            <p>dddd</p>
+        <div class="flex w-full gap-12">
+          <aside class="h-screen sticky top-24 w-[28%] border-r">
+            <ul className="">
+              {
+                sideTitle?.map((item,idx)=>(
+                  <li className="font-semibold py-2 capitalize cursor-pointer hover:text-primary" key={idx}><span className="font-normal mr-1">{idx+1}.</span> {item.title}</li>
+                ))
+              }
+            </ul>
           </aside>
 
-          <main className=" flex-1">
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
-            <p>dddd</p>
+          <main className=" flex-1 ">
+            
           </main>
         </div>
       </Container>
@@ -62,3 +26,29 @@ const Content = () => {
 };
 
 export default Content;
+
+
+
+
+const sideTitle = [
+  {
+    title: "money for your business?",
+    tag: ""
+  },
+  {
+    title: "Fast business loans",
+    tag: ""
+  },
+  {
+    title: "Realize your dream",
+    tag: ""
+  },
+  {
+    title: "Expand The Business",
+    tag: ""
+  },
+  {
+    title: "Interest on business loans",
+    tag: ""
+  },
+]
